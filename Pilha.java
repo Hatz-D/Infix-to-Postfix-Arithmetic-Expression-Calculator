0cbcee4b0c0c387075eb08ddfecee01af17d2d05
Hatz-D <124005927+Hatz-D@users.noreply.github.com>
2023-04-06 16:09:48 -0300

    Add files via upload

---
+++ Pilha.java
@@ -0,0 +1,69 @@
+/* COMPONENTES DO GRUPO
+ * NOME: Diogo Lourenzon Hatz - TIA: 32247389
+ * NOME: Leila Akina Ino - TIA: 32261128
+ * NOME: Livia Alabarse dos Santos - TIA: 32264429
+ * NOME: Pedro Henrique Araujo Farias - TIA: 32246994
+ */
+@SuppressWarnings("unchecked")
+
+public class Pilha<T>{
+    static final int CAPACIDADE_PILHA = 128;
+    private int contador;
+    private T[] pilha;
+
+    public Pilha() {
+        this(CAPACIDADE_PILHA);
+    }
+
+    public Pilha(int capacidade){
+        this.pilha = (T[]) new Object[capacidade];
+        contador = 0;
+    }
+
+    public void push(T a){
+        if(isFull()){
+            throw new IllegalArgumentException("The stack is full");
+        }
+        pilha[contador] = a;
+        contador++;
+    }
+
+    public T pop(){
+        if(isEmpty()){
+            throw new IllegalArgumentException("The stack is empty!");
+        }
+        contador--;
+        T topo = pilha[contador];
+        pilha[contador] = null;
+        return topo;
+    }
+
+    public T top(){
+        if(isEmpty()){
+            throw new IllegalArgumentException("The stack is empty!");
+        }
+        return pilha[contador-1];
+    }
+
+    public int count(){
+        return contador;
+    }
+
+    public boolean isFull(){
+        return contador == pilha.length;
+    }
+
+    public boolean isEmpty(){
+        return contador == 0;
+    }
+
+    public void clear(){
+        while(!isEmpty()){
+            pop();
+        }
+    }
+
+    public int size(){
+        return pilha.length;
+    }
+}
